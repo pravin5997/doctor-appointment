0f93cd61210f4ed99d686ac024052f35e16df6eb
Pravin <dabhipravin.dp12@gmail.com>
2020-10-08 12:34:38 +0530

    update apps.py file

--- patient_care/apps.py
+++ patient_care/apps.py
@@ -2,7 +2,4 @@ from django.apps import AppConfig
 
 
 class PatientCareConfig(AppConfig):
-    name = 'patient_care'
-
-    def ready(self):
-        import patient_care.signals
+    name = 'patient_care' 
