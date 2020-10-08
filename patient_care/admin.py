3ab220ed1ce340cb327c0ac50831eb60b3319111
Pravin <dabhipravin.dp12@gmail.com>
2020-10-08 12:28:47 +0530

    create api for doctor appointment

--- patient_care/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Patient, PatientProfile, Doctor, BookDoctor, SearchAttribute, SearchAttributeValue
-# Register your models here.
-admin.site.register(Patient)
-admin.site.register(PatientProfile)
-admin.site.register(Doctor)
-admin.site.register(BookDoctor)
-admin.site.register(SearchAttribute)
-admin.site.register(SearchAttributeValue)
--- patient_care/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class PatientCareConfig(AppConfig):
-    name = 'patient_care'
-
-    def ready(self):
-        import patient_care.signals
